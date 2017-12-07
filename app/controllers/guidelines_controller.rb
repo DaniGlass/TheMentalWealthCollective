ba8ab8d760741f8aad43728fd4ab99fe35942dcc
DaniGlass <danielleglass@me.com>
2017-12-07 11:28:38 -0800

    set up controllers, routes, and views for classes, blog, mission, and guidelines

---
+++ app/controllers/guidelines_controller.rb
@@ -0,0 +1,2 @@
+class GuidelinesController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'mission/index'
+
+  resources :blog, :classes, :guidelines, :only => [:index]
+
+  root 'mission#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
