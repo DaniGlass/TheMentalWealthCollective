b584bd690bde1c31519a892eb91b9221e1a742fd
DaniGlass <danielleglass@me.com>
2017-12-13 13:54:43 -0800

    use parsed response as html

--- app/models/medium_adapter.rb
+++ app/models/medium_adapter.rb
@@ -7,14 +7,30 @@ module MediumAdapter
  # Returns a parsed json object of the request
     def posts
       url = "https://medium.com/feed/@ccmarie.clark"
-      response = HTTP.get(url).to_s
-      @doc = Nokogiri::XML(response)
-      @all_posts = @doc.xpath("//item")
+      response = HTTP.get(url)
+      @doc = Nokogiri::XML(response, nil, 'UTF-8')
+      @posts_titles = @doc.css("title")
+      @posts_dates = @doc.css("pubDate")
+      # content and images aren't working
+      @posts_content = @doc.css("p")
+     end
+
+    def show_title1
+      @title = @posts_titles[1].to_s
+      @title = @title.gsub("<title>", "<h2>")
+      @title = @title.gsub("</title>", "</h2>")
+      @title
     end
 
-    def show_post
-      most_recent = @all_posts[0].to_s
-      most_recent
+    def show_date1
+      @posts_dates[0].to_s
     end
+
+    def show_content1
+      # @doc
+      @posts_content
+      # "wtf"
+    end
+
   end
 end
