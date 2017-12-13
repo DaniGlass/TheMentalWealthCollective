386432ab6580a0e2867f5d76f27a215b26702d4b
DaniGlass <danielleglass@me.com>
2017-12-13 11:54:48 -0800

    adding nokogiri to parse xml response

--- app/models/medium_adapter.rb
+++ app/models/medium_adapter.rb
@@ -6,9 +6,10 @@ module MediumAdapter
 
  # Returns a parsed json object of the request
     def posts
-      url = "https://api.medium.com/v1/users/19b66064ca7fad2b45dfa5b40887465259b736f92db6ff4a8ca8c48519612b2d4/publications"
-      response = HTTP.auth(ENV["AUTHORIZATION"]).get(url)
-      response.parse
+      url = "https://medium.com/feed/@ccmarie.clark"
+      response = HTTP.get(url).to_s
+      @doc = Nokogiri::XML(response)
+      @doc.xpath("//item")
     end
   end
 end
