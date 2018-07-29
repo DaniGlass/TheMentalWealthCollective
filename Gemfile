29a05055075636093cdea4910df45c4d7063e981
DaniGlass <danielleglass@me.com>
2018-07-29 15:37:44 -0700

    hide variables

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ gem 'devise'
 gem "http"
 gem 'pg', '~> 0.18'
 gem 'gibbon', '~> 3.0.0'
+gem 'dotenv-rails'
 
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
--- app/controllers/subscriber_controller.rb
+++ app/controllers/subscriber_controller.rb
@@ -7,7 +7,7 @@ class SubscriberController < ApplicationController
     # render plain: params[:subscriber].inspect
     @subscriber = Subscriber.new(subscriber_params)
 
-    gibbon = Gibbon::Request.new(api_key: "29e6f01ac72c7bcae25531daf1dbda03-us18")
+    gibbon = Gibbon::Request.new(api_key: ENV["MAILCHIMP_API"])
 
     if @subscriber.save
       gibbon.lists('691fe0e391').members.create(body: {email_address: "#{@subscriber[:email]}", status: "subscribed"})
